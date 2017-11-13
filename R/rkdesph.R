54aaf3542b7adfcabbed3949626d9f71c0c9144a
Andreas Dyreborg Christoffersen <andreas@math.aau.dk>
2017-11-12 22:40:11 -0500

    rkdesph modified to accept pps objects instead of matrices

--- R/rkdesph.R
+++ R/rkdesph.R
@@ -1,9 +1,6 @@
 #' Simulate from Spherical kernel density
 #'
 #' Function for simulating under a spherical kernel density.
-#' @param np Either a single number indicating the exact number of points in the simulated pattern
-#' or "Poisson" in which case the number of points in each simulated pattern will be Poisson distributed
-#' with mean equal to the number of points in the observed pattern
 #' @param kappa A consentration parameter
 #' @param obs A matrix with rows constituting either cartesian or spherical coordinates of the points.
 #' If \code{obs} is spicified by spherical coordinates \code{sph_coords} must be TRUE.
@@ -11,7 +8,9 @@
 #' Default is TRUE.
 #' The points need not have norm 1.
 #' @param nsim The number of point patterns to be simulated.
-#' @param sph_coords A logical value indicating wether the data supplied are in spherical or cartesian coordinates.
+#' @param np Either a single number indicating the exact number of points in the simulated pattern
+#' or "Poisson" in which case the number of points in each simulated pattern will be Poisson distributed
+#' with mean equal to the number of points in the observed pattern.
 #' @param drop A logical value indicating whether the output should be a \code{\link{sp2}} object of a list of \code{\link{sp2}} objects.
 #' This only has an effect if \code{nsim == 1}.
 #' @return A \code{\link{sp2}} object or a list of \code{\link{sp2}} object with length equal to \code{nsim}.
@@ -21,19 +20,19 @@
 #' @importFrom movMF rmovMF
 #' @importFrom stats rpois
 #' @export
-rkdesph <- function(np, kappa, obs, nsim, sph_coords = TRUE, drop = TRUE){
-  if(sph_coords){
-    obs <- t(apply(as.data.frame(obs), 1, sph2car))
-  }
+rkdesph <- function(kappa, obs, nsim = 1, np = "Poisson", drop = TRUE){
+  stopifnot(verifyclass(obs, "pps"))
+  obs_car <- as.matrix(sph2car(as.data.frame(obs$data)))
   out <- list()
-  if(np == "Poisson"){
-    np <- rpois(nsim, nrow(obs))
+  if (np == "Poisson"){
+    np <- rpois(nsim, npoints(obs))
   }
-  for(i in 1:nsim){
-    tmp <- as.data.frame(rmovMF(np[i], kappa * obs)[, 1:3])
-    out[[i]] <- sp2(t(apply(tmp, 1, car2sph, rep = "1"))[, c("lat", "long")])
+  for (i in 1:nsim){
+    tmp <- as.data.frame(rmovMF(np[i], kappa * obs_car)[, 1:3])
+    tmp2 <- do.call(rbind, apply(tmp, 1, car2sph, rep = "sph"))
+    out[[i]] <- pps(x = tmp2$long, y = tmp2$lat, window = obs$window)
   }
-  if(drop & nsim == 1){
+  if (drop & nsim == 1) {
     out <- out[[1]]
   }
   return(out)
